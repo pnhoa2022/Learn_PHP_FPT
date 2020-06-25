8687429382775b68a64bdc27e4aba250df528f85
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-06-25 09:08:05 +0700

    first commit

---
+++ Session_1/example_1_1.php
@@ -0,0 +1,15 @@
+<?php
+    echo "Xin chào thế giới"; echo "<br>";
+
+    $myCounter = 1;
+    $myString = "Hello";
+    $myArray = array("one", "Two", "Three");
+
+    echo $myCounter; echo "<br>";
+    echo $myString; echo "<br>";
+    echo $myArray[0]; echo "<br>";
+
+    echo rand(1, 10); echo "<br>";
+
+    echo 'This is a <strong>test</strong>!'; echo "<br>";
+?>
