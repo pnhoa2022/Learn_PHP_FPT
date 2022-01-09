d066b39a04fe36555957242884eff42ce10be48a
Hiếu Nguyễn Đình <hieund@kaopiz.com>
2022-01-09 10:58:53 +0700

    php artisan make:import TransactionsImport --model=Transaction

--- Excel_Maatwebsite/app/Imports/TransactionsImport.php
+++ Excel_Maatwebsite/app/Imports/TransactionsImport.php
@@ -15,7 +15,11 @@ class TransactionsImport implements ToModel
     public function model(array $row)
     {
         return new Transaction([
-            //
+            'name_on_card'     => $row[0],
+            'card_no'    => $row[1],
+            'exp_month'    => $row[2],
+            'exp_year'    => $row[3],
+            'cvv'    => $row[4],
         ]);
     }
 }
