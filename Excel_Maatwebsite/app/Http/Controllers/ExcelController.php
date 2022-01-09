514a2a16209c78c5f44f6dabdb1392c23a4816d3
Hiếu Nguyễn Đình <hieund@kaopiz.com>
2022-01-09 11:09:16 +0700

    php artisan make:controller ExcelController

--- Excel_Maatwebsite/app/Http/Controllers/ExcelController.php
+++ Excel_Maatwebsite/app/Http/Controllers/ExcelController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Exports\TransactionsExport;
+use App\Imports\TransactionsImport;
 
 class ExcelController extends Controller
 {
-    //
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function importExportView()
+    {
+        return view('excel.index');
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function exportExcel($type)
+    {
+        return \Excel::download(new TransactionsExport, 'transactions.'.$type);
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function importExcel(Request $request)
+    {
+        \Excel::import(new TransactionsImport,$request->import_file);
+
+        \Session::put('success', 'Your file is imported successfully in database.');
+
+        return back();
+    }
 }
