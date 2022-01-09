48d43b7d736dd19d8052a39dddd3f8caee6ef5e2
Hiếu Nguyễn Đình <hieund@kaopiz.com>
2022-01-09 11:51:58 +0700

    update: ExcelController:
    use Maatwebsite\Excel\Facades\Excel;

--- Excel_Maatwebsite/app/Http/Controllers/ExcelController.php
+++ Excel_Maatwebsite/app/Http/Controllers/ExcelController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Exports\TransactionsExport;
 use App\Imports\TransactionsImport;
 
+use Maatwebsite\Excel\Facades\Excel;
+
 class ExcelController extends Controller
 {
     /**
@@ -22,7 +24,7 @@ class ExcelController extends Controller
      */
     public function exportExcel($type)
     {
-        return \Excel::download(new TransactionsExport, 'transactions.'.$type);
+        return Excel::download(new TransactionsExport, 'transactions.'.$type);
     }
 
     /**
@@ -30,7 +32,7 @@ class ExcelController extends Controller
      */
     public function importExcel(Request $request)
     {
-        \Excel::import(new TransactionsImport,$request->import_file);
+        Excel::import(new TransactionsImport,$request->import_file);
 
         \Session::put('success', 'Your file is imported successfully in database.');
 
