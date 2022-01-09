d1c3351b52a3afcb8cbaf03a31e249664e3fb430
Hiếu Nguyễn Đình <hieund@kaopiz.com>
2022-01-09 11:53:30 +0700

    update: ExcelController:
    format code

--- Excel_Maatwebsite/app/Http/Controllers/ExcelController.php
+++ Excel_Maatwebsite/app/Http/Controllers/ExcelController.php
@@ -11,25 +11,17 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ExcelController extends Controller
 {
-    /**
-     * @return \Illuminate\Support\Collection
-     */
     public function importExportView()
     {
         return view('excel.index');
     }
 
-    /**
-     * @return \Illuminate\Support\Collection
-     */
     public function exportExcel($type)
     {
         return Excel::download(new TransactionsExport, 'transactions.'.$type);
     }
 
-    /**
-     * @return \Illuminate\Support\Collection
-     */
+
     public function importExcel(Request $request)
     {
         Excel::import(new TransactionsImport,$request->import_file);
