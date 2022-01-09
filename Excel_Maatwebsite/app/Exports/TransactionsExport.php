ba47b070955634cc1a23634b2b33aff4c2e81caf
Hiếu Nguyễn Đình <hieund@kaopiz.com>
2022-01-09 11:03:29 +0700

    php artisan make:export TransactionsExport --model=Transaction

--- Excel_Maatwebsite/app/Exports/TransactionsExport.php
+++ Excel_Maatwebsite/app/Exports/TransactionsExport.php
@@ -14,4 +14,26 @@ class TransactionsExport implements FromCollection
     {
         return Transaction::all();
     }
+
+    public function headings(): array
+    {
+        return [
+            'Name On Card',
+            'Card No.',
+            'Exp Month',
+            'Exp. Year',
+            'CVV',
+        ];
+    }
+
+    public function map($transaction): array
+    {
+        return [
+            $transaction->name_on_card,
+            'XXXXXXXXXXXX' . substr($transaction->card_no, -4, 4),
+            $transaction->exp_month,
+            $transaction->exp_year,
+            $transaction->cvv,
+        ];
+    }
 }
